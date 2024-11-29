0cc53d873583cec4186c2a825c509d0321f16eec
ponponumi <ponponumi@gmail.com>
2024-11-30 00:55:59 +0900

    コンストラクタを追加

--- src/Title.php
+++ src/Title.php
@@ -7,4 +7,11 @@ class Title
     public string $siteName = "";
     public string $pageName = "";
     public string $markOff = "";
+
+    public function __construct(string $siteName,string $pageName="",string $markoff=" | ")
+    {
+        $this->siteName = $siteName;
+        $this->pageName = $pageName;
+        $this->markOff = $markoff;
+    }
 }
