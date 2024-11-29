6f45ef7da820a1a93cafc4d809032e7135771c7d
ponponumi <ponponumi@gmail.com>
2024-11-30 02:15:51 +0900

    正しく出力する事を確認

--- parts/header.php
+++ parts/header.php
@@ -10,18 +10,21 @@ $title = new Title("<テストサイト>",$page);
 $title->markOffChange(" - ");
 
 $themeColor = new ThemeColor("#ad1");
-$themeColor->colorChange("");
 $themeColor->cssVariableNameChange("--theme-color");
 
 ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
+    <?php
+
+    $themeColor->metaEcho();
+    $themeColor->styleEcho();
+
+    ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?= $title->htmlGet() ?>
-    <?= $themeColor->metaGet() ?>
-    <?= $themeColor->styleGet() ?>
 </head>
 <body>
     <h1><?= $title->headingGet() ?></h1>
