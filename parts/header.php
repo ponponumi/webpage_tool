ae26352fdf634a4d4b21246b091d32870fd84bac
ponponumi <ponponumi@gmail.com>
2024-11-30 01:32:57 +0900

    エラーが発生

--- parts/header.php
+++ parts/header.php
@@ -13,7 +13,7 @@ $title = new Title("<テストサイト>",$page);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php $title->echo() ?></title>
+    <?php $title->htmlEcho() ?>
 </head>
 <body>
     <h1><?= $title->headingGet() ?></h1>
