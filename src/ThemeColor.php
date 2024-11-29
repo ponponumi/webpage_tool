a24d76143bf1fb76385d21c4293de30a49a6ec98
ponponumi <ponponumi@gmail.com>
2024-11-30 01:51:21 +0900

    色を変えるメソッドを作成

--- src/ThemeColor.php
+++ src/ThemeColor.php
@@ -10,4 +10,20 @@ class ThemeColor
     {
         $this->color = $color;
     }
+
+    public function colorChange(string $color="")
+    {
+        // 色を変える
+        if(preg_match('/^#[0-9a-fA-F]{6}$/', $color) === 1){
+            // 通常の16進数なら
+            $this->color = $color;
+        }elseif(preg_match('/^#[0-9a-fA-F]{3}$/', $color) === 1){
+            // 3桁表記なら
+            $colorSplit = str_split($color);
+            $color = "#" . $colorSplit[1] . $colorSplit[1] . $colorSplit[2] . $colorSplit[2] . $colorSplit[3] . $colorSplit[3];
+            $this->color = $color;
+        }
+
+        $this->color = $color;
+    }
 }
