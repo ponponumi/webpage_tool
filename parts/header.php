93377333a65e3877d427031b5bfa174de6b8e081
ponponumi <ponponumi@gmail.com>
2024-11-30 01:24:40 +0900

    ライブラリを呼び出すように変更

--- parts/header.php
+++ parts/header.php
@@ -2,12 +2,18 @@
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
+use Ponponumi\WebpageTool\Title;
+
+$page = isset($page) ? $page : "";
+$title = new Title("テストサイト",$page);
+
 ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?= $title->get() ?></title>
 </head>
 <body>
+    <h1><?= $title->headingGet() ?></h1>
